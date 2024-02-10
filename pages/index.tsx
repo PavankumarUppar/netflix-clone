472a561e00d5eef1ecf4d33d18cef4db70970fbb
Pavankumar Uppar <pavankumar.ai20@bmsce.ac.in>
2024-02-10 19:16:50 +0530

    navbar added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
-import { redirect } from "next/dist/server/api-utils";
+
 
 export async function  getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,14 +23,12 @@ export async function  getServerSideProps(context: NextPageContext) {
 
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  
 
 
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as: { user?.name }</p>
-      <button onClick={() => signOut()} className="h-10 w-full bg-white">Logout!</button>
+      <Navbar />
     </>
   );
 }
