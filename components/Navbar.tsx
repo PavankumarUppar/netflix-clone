ffe0d868f310b24032e2494a07966e8be6b4798d
Pavankumar Uppar <pavankumar.ai20@bmsce.ac.in>
2024-02-10 21:15:57 +0530

    navbar is finished

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,14 @@ import AccountMenu from "./AccountMenu";
 
 const Navbar = () => {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const [showAccountMenu, setShowAccountMenu] = useState(false);
 
     const toggleMobileMenu = useCallback(() => {
         setShowMobileMenu((current) => !current)
+    }, []);
+
+    const toggleAccountMenu = useCallback(() => {
+        setShowAccountMenu((current) => !current)
     }, []);
 
     return (
@@ -45,7 +50,7 @@ const Navbar = () => {
                 </div>
                 <div onClick={toggleMobileMenu} className="lg:hidden flex flex-wor items-center gap-2 ml-8 cursor-pointer relative">
                     <p className="text-white text-sm">Browse</p>
-                    <BsChevronDown className="text-white transition" />
+                    <BsChevronDown className= {`text-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`} />
                     <MobileMenu visible={showMobileMenu}/>
                 </div>
                 <div className="flex flex-row ml-auto gap-7 items-center">
@@ -55,12 +60,13 @@ const Navbar = () => {
                     <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
                         <BsBell />
                     </div>
-                    <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+
+                    <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
                         <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
                             <img src="/images/default-blue.png" alt="" />
                         </div>
-                        <BsChevronDown className="text-white transition" />
-                        <AccountMenu visible />
+                        <BsChevronDown className= {`text-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`} />
+                        <AccountMenu visible={showAccountMenu} />
                     </div>
                 </div>
             </div>
